090d269a8da9f82253a0f394128e3583b0b8589a
SabbirHossain07 <sabbirsopnil07@gmail.com>
2022-06-05 20:55:46 +0600

    Initial Commit

---
+++ ResponsiveUI2/ResponsiveUI2App.swift
@@ -0,0 +1,17 @@
+//
+//  ResponsiveUI2App.swift
+//  ResponsiveUI2
+//
+//  Created by Sopnil Sohan on 5/6/22.
+//
+
+import SwiftUI
+
+@main
+struct ResponsiveUI2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
